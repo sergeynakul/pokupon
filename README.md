fb288b044770e5c8cd3b49d43432c17e3eb0445d
Sergey <nakul.sv@gmail.com>
2018-04-01 21:47:18 +0300

    finish

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Запуск
 
 1. Клонировать репозиторий
-2. Установить библиотеки: whenever, mail, faraday
-3. Запустить в терминале команду - whenever --update-crontab
+2. В файле check.rb заменить "/home/newHomeDir/rails/ruby/" на свой путь.
+3. В файле schedule.rb заменить путь "~/rails/ruby/" на свой путь.
+4. Установить библиотеки: whenever, mail, faraday
+5. Запустить в терминале команду - whenever --update-crontab
--- check.rb
+++ check.rb
@@ -13,31 +13,20 @@ Mail.defaults do
   enable_starttls_auto: true
 end
 
-path = File.expand_path(".")
-
 site1 = 'https://pokupon.ua/'
 site2 = 'https://partner.pokupon.ua/'
 sites = [site1, site2]
 
 sites.each do |site|
-	old_status = File.read("#{path}/status#{sites.index(site)}.txt").chomp
+	path = "/home/newHomeDir/rails/ruby/pokupon/status#{sites.index(site)}.txt"
+	old_status = File.read(path).chomp
 	response = Faraday.get site
-	File.open("#{path}/status#{sites.index(site)}.txt", 'w') { |file| file.write("#{response.status}") }
+	File.open(path, 'w') { |file| file.write("#{response.status}") }
 
 	if response.status != 200 && response.status != old_status
 		Mail.deliver do
 		  from    'ror.test.e@gmail.com'
-		  to      'nakul.sv@gmail.com'
-		  subject site
-		  body    "Status - #{response.status}. Error - " # как указать ошибку
-		end
-	end
-
-	# для тестирования
-	if response.status == 200
-		Mail.deliver do
-		  from    'ror.test.e@gmail.com'
-		  to      'nakul.sv@gmail.com'
+		  to      'alert@pokupon.ua'
 		  subject site
 		  body    "Status - #{response.status}."
 		end
